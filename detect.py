cb975574739b7b527a4a51f25a81b955aefb063d
Timo Philipp <73222615+philippti@users.noreply.github.com>
2023-03-10 11:51:04 +0100

    function for detection in detect.py
    now returns True or False in case of fire

--- detect.py
+++ detect.py
@@ -1,9 +1,24 @@
 from ultralytics import YOLO
 
-model = YOLO("runs/detect/train9/weights/best.pt")
+def detect_fire(path_to_model: str, path_to_image: str) -> str :
 
-results = model("test.jpg")
+    model = YOLO(path_to_model)
 
-for r in results:
-    for c in r.boxes.cls:
-        print(model.names[int(c)])
+    results = model(path_to_image)
+
+    detected_classes = []
+    for r in results:
+        for c in r.boxes.cls:
+            if model.names[int(c)] == "fire":
+                print("!"*20, "FIRE DETECTED", "!"*20)
+                return True
+            else:
+                continue
+    
+
+if __name__ == "__main__":
+
+    model_path = "/home/projects/waldbraende/runs/detect/train9/weights/best.pt"
+    test_image = "/home/projects/waldbraende/test.jpg"
+
+    detect_fire(path_to_model=model_path, path_to_image = test_image)
